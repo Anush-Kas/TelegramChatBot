c3d36caecf3ca943e9dbc7eb564714d3289cd7ad
Anush-Kas <anushkasparova@gmail.com>
2022-09-30 10:22:20 +0300

    Телеграмм бот и все что с ним связанно

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import telebot
 import requests
 import json
 from key import TOKEN
-impor
 
 
 bot = telebot.TeleBot(TOKEN)
@@ -29,6 +28,38 @@ def values(message: telebot.types.Message):
         text = "\n".join((text, key, ))
     bot.reply_to(message, text)
 
+
+@bot.message_handler(content_types=["text", ])
+def convert(message: telebot.types.Message):
+    values = message.text.split(" ")
+
+    if len(values) > 3:
+        raise ConvertionException(f"Что-то много информации, можно чуть-чуть поменьше")
+
+    base, currency, amount = values
+
+    if base == currency:
+        raise ConvertionException("Мне кажется ты знаешь ответ на этот запрос)")
+
+    try:
+        base_ticker = keys[base]
+    except KeyError:
+        raise ConvertionException(f"Не понятная валюта {base} для меня")
+
+    try:
+        currency_ticker = keys[currency]
+    except KeyError:
+        raise ConvertionException(f"Не понятна валюта {currency} для меня")
+
+    try:
+        amount = float(amount)
+    except ValueError:
+        raise ConvertionException(f"Мне кажется это {amount} не похоже на цифру")
+
+    r = requests.get(f"https://min-api.cryptocompare.com/data/price?fsym={base_ticker}&tsyms={currency_ticker}")
+    total_currency = json.loads(r.content)[keys[currency]]
+    text = f"Цена {amount} {base} в {currency} - {float(amount) * total_currency}"
+    bot.send_message(message.chat.id, text)
 
 
 # Press the green button in the gutter to run the script.
